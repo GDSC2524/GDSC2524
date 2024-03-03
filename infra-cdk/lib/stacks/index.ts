216bbc95ba4a4f21d4a1aa4b8f39bc2dd6cd13f3
SophieNguyen113 <sophiengocnguyen113@gmail.com>
2024-03-03 02:20:46 -0500

    Fixed bugs

--- infra-cdk/lib/stacks/index.ts
+++ infra-cdk/lib/stacks/index.ts
@@ -1,7 +1,6 @@
 export * from './DevIAMStack';
 export * from './IAMStack';
 export * from './NextJsStack';
-export * from './MessagesTableStack';
 export * from './ReportsTableStack';
 export * from './ReportsBucketStack';
 export * from './PipelineStack';
