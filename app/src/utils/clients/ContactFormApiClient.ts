2600b6d6133b2e9943e2df0fd90d60df1c51adfe
Nguyen Huu Phuc Toan <toantoan123toantoan@gmail.com>
2024-03-17 17:23:29 -0400

    This is commit for method get by id for contactform viewing

--- app/src/utils/clients/ContactFormApiClient.ts
+++ app/src/utils/clients/ContactFormApiClient.ts
@@ -1,7 +1,9 @@
 import { HttpMethod, IContactFormClient, IContactForm } from '@/models';
 import { ICreateContactFormResponse } from '@/pages/api/contactForm/create';
+import { IGetContactFormResponse } from '@/pages/api/contactForm/[id]';
 
 const CREATE_CONTACTFORM_ENDPOINT = '/api/contactForm/create';
+const GET_CONTACTFORM_BASE_ENDPOINT = '/api/contactForm/get';
 
 /** Client to interact with report API */
 // TODO implement error handling
@@ -16,6 +18,13 @@ export class ContactFormApiClient implements IContactFormClient {
             },
         });
         const json: ICreateContactFormResponse = await response.json();
+
+        return json.contactForm;
+    }
+
+    async getContactForm(contactFormId: string) {
+        const response = await fetch(`${GET_CONTACTFORM_BASE_ENDPOINT}/${contactFormId}`);
+        const json: IGetContactFormResponse = await response.json();
 
         return json.contactForm;
     }
